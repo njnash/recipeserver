55c0233da3a5bf59eed4bf69097cc47a5cb1b4b3
Richard Nash <rvnash@gmail.com>
2015-09-08 14:12:21 +0000

    app.js edited online ... need next commit to pick up restart.js

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var RecipeManager = require('./RecipeManager');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var recipe = require('./routes/recipe');
+var restart = require('./routes/restart');
 
 var app = express();
 global.rm = new RecipeManager('1qfK7ULjezDNXlQRMW55niFDRHz9WEjC4OxYGDgYlSNA');
@@ -30,6 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/recipe', recipe);
+app.use('/restart', restart);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
